b09bc3fb4b574b04189633a41861d12e9cb413f5
baocvt <bao.chu@nokasoft.com.vn>
2026-04-20 17:21:23 +0700

    fix

--- app/Http/Controllers/Client/ClientController.php
+++ app/Http/Controllers/Client/ClientController.php
@@ -16,7 +16,7 @@ class ClientController extends Controller
             ->latest('id')
             ->limit(5)->get();
 
-        $categories = Category::get(['id', 'name', 'category_image'])->latest()->paginate(10);
+        $categories = Category::latest()->paginate(10, ['id', 'name', 'category_image']);
 
         $featured_products = $this->getFeaturedProduct(8, 'favorites');
 
